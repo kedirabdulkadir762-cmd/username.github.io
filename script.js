bc33af76caf4bf6184054808ec1fe8dbd4940424
kedirabdulkadir762-cmd <kedirabdulkadir762@gmail.com>
2026-03-09 21:22:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function scrollToSection(id){
 
+document.getElementById(id).scrollIntoView({
+behavior:'smooth'
+})
+
+}
+
+function changeLanguage(lang){
+
+if(lang=="or"){
+
+document.querySelector(".site-name").innerText="Abdulkadir Math Academy"
+
+}
+
+else if(lang=="am"){
+
+document.querySelector(".site-name").innerText="Abdulkadir Math Academy"
+
+}
+
+else{
+
+document.querySelector(".site-name").innerText="Abdulkadir Math Academy"
+
+}
+
+}
